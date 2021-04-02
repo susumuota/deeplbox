56ed9618a52d0b96ec9b45df82c8db338574d295
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2021-04-02 18:22:32 +0900

    delete manifest.min.json

--- makezip.sh
+++ makezip.sh
@@ -3,12 +3,5 @@
 # make zip file for Google Web Store
 
 ZIP_FILE="deeplkey-latest.zip"
-TMP_MANIFEST="manifest.org.json"
 
-cp -p manifest.json "$TMP_MANIFEST"
-cp -p manifest.min.json manifest.json
-
-zip "$ZIP_FILE" manifest.json background.js deepl.js translation.html translation.js translation.css icons/icon*.png
-
-cp -p "$TMP_MANIFEST" manifest.json
-rm "$TMP_MANIFEST"
+zip "$ZIP_FILE" manifest.json background.js deepl.js selection.js translation.html translation.js translation.css icons/icon*.png
