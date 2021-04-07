8a4736e7cebdc0947fb5222f32e610a1356e34fc
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2021-04-07 20:45:22 +0900

    add default.js and popup files

--- makezip.sh
+++ makezip.sh
@@ -4,4 +4,4 @@
 
 ZIP_FILE="deeplkey-latest.zip"
 
-zip "$ZIP_FILE" manifest.json background.js deepl.js selection.js translation.html translation.js translation.css icons/icon*.png
+zip "$ZIP_FILE" manifest.json background.js default.js deepl.js selection.js popup.html popup.js popup.css translation.html translation.js translation.css icons/*.png
